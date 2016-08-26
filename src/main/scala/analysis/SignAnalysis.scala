d1fe1e114b67788ecb00bbbbbb7f045365913f36
Fabian Ritter <fabian.ritter@cs.uni-saarland.de>
2016-08-26 12:38:13 +0200

    Implement join for SignAnalysis

--- src/main/scala/analysis/SignAnalysis.scala
+++ src/main/scala/analysis/SignAnalysis.scala
@@ -2,7 +2,50 @@ package analysis
 
 import cfg._
 
-sealed abstract class SignVal
+trait AbstractVal[A] {
+  def join(other: A): A
+
+  // def add(other: A): A
+  // def sub(other: A): A
+  // def mul(other: A): A
+  // def div(other: A): A
+  //
+  // def slt(other: A): A
+}
+
+sealed abstract class SignVal extends AbstractVal[SignVal] {
+  override def join(other: SignVal): SignVal = {
+    (this, other) match {
+      case (TOP(), _) | (_, TOP()) => TOP()
+      case (BOT(), o) => o
+      case (t, BOT()) => t
+      case (t, o) if t == o => t
+
+      case (EZ(), GZ()) => GEZ()
+      case (EZ(), LZ()) => LEZ()
+      case (EZ(), LEZ()) => LEZ()
+      case (EZ(), GEZ()) => GEZ()
+      case (EZ(), NEZ()) => TOP()
+
+      case (GZ(), LZ()) => NEZ()
+      case (GZ(), LEZ()) => TOP()
+      case (GZ(), GEZ()) => GEZ()
+      case (GZ(), NEZ()) => NEZ()
+
+      case (LZ(), LEZ()) => LEZ()
+      case (LZ(), GEZ()) => TOP()
+      case (LZ(), NEZ()) => NEZ()
+
+      case (LEZ(), GEZ()) => TOP()
+      case (LEZ(), NEZ()) => TOP()
+
+      case (GEZ(), NEZ()) => TOP()
+
+      case (t, o) => o join t
+    }
+  }
+}
+
 case class BOT() extends SignVal
 case class EZ() extends SignVal
 case class GZ() extends SignVal
@@ -17,6 +60,8 @@ case class TOP() extends SignVal
  * values in the function.
  */
 class SignAnalysis(fun: Function) extends ValueAnalysis[SignVal](fun) {
+
+
   override def run(): Unit = {
     populateSymbolTable(new BOT())
 
@@ -27,8 +72,7 @@ class SignAnalysis(fun: Function) extends ValueAnalysis[SignVal](fun) {
       GZ()
     } else if (x < 0) {
       LZ()
-    }
-    else {
+    } else {
       EZ()
     }
   }
