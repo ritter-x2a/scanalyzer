25c52fca3e4a2f6057442e637564193c9349b24f
Fabian Ritter <fabian.ritter@cs.uni-saarland.de>
2016-08-29 11:36:16 +0200

    Add arithmetic test and div0 as parser tests

--- src/test/scala/Parser.scala
+++ src/test/scala/Parser.scala
@@ -16,6 +16,14 @@ class ParserSpec extends FlatSpec with Matchers {
     performParseTest("examplefiles/bigint.cfg")
   }
 
+  it should "parse arithmetic operations correctly" in {
+    performParseTest("examplefiles/arithmetic.cfg")
+  }
+
+  it should "parse divisions by zero correctly" in {
+    performParseTest("examplefiles/neg_divzero.cfg")
+  }
+
   it should "throw ParserException if an undefined name is used" in {
     a [ParserException] should be thrownBy {
       performParseTest("examplefiles/neg01.cfg")
